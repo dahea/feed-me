e1c35e978cb7795602a5f6379e0271008a817c24
dahea <dahea.an@gmail.com>
2019-06-25 00:36:50 -0400

    add state to App and TickerTable

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import './App.css';
  * - Use data returned from feed to render and update table rows with relevant data
  * - Allow a symbol to be unsubscribed from
  */
-const symbols = ['o','pear','moth']
 
 const feed = {
   subscribe: function(symbol) {
       // subscribe to a socket
       console.log(`now subscribed to ${symbol}`)
-      symbols.push(symbol)
-      console.log(symbols)
+      // symbols.push(symbol)
+      // console.log(symbols)
   },
   unsubscribe: function(symbol) {
       // unsubscribe from a socket
       console.log(`now unsubscribed from ${symbol}`)
-      symbols.splice(symbols.findIndex((item) => item === symbol),1)
-      console.log(symbols)
+      // symbols.splice(symbols.findIndex((item) => item === symbol),1)
+      // console.log(symbols)
 
   },
   onChange: function(callback) {
@@ -27,62 +26,74 @@ const feed = {
   }
 };
 
-// my stuff
-let ticker
-const inputHandleChange = (e) => {
-  ticker = e.target.value.toUpperCase()
-}
-
-const handleSubscribe = (e) => {
-  e.preventDefault();
-  feed.subscribe(ticker)
-}
-
-const handleUnSubscribe = (e) => {
-  feed.unsubscribe(e.target.dataset.symbol)
-}
-
 class TickerTable extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      symbols: ['o','pear','moth']
+    }
+  }
+  handleUnSubscribe = (e) => {
+    feed.unsubscribe(e.target.dataset.symbol)
+  }
+
   render(){
     return (
       <tbody>
         {
-          symbols.map((symbol, index) => {
+          this.state.symbols.map((symbol, index) => {
             return (
               <tr key={index}>
                 <td>{symbol}</td>
                 <td>{symbol} start</td>
                 <td>{symbol} end</td>
-                <td><button data-symbol={symbol} onClick={handleUnSubscribe}>Unsubscribe from {symbol}</button></td>
+                <td><button data-symbol={symbol} onClick={this.handleUnSubscribe}>Unsubscribe from {symbol}</button></td>
               </tr>
             )
-         })
+          })
         }
       </tbody>
     )
   }
 }
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {ticker: ''}
+  }
+  
+  inputHandleChange = (e) => {
+    this.setState({ticker: e.target.value.toUpperCase()})
+  }
 
-const App = () => (
-  <div>
-    <form onSubmit={handleSubscribe}>
-      <input type="text" onChange={inputHandleChange}/>
-      <button type="submit">Subscribe</button>
-    </form>
-      <table>
-          <thead>
-              {/* blah blah table header */}
-              <tr>
-                  <th>Symbol</th>
-                  <th>Open</th>
-                  <th>Last</th>
-                  <th>Unsubscribe</th>
-              </tr>
-          </thead>
-            <TickerTable />
-      </table>
-  </div>
-)
+  handleSubscribe = (e) => {
+    e.preventDefault();
+    feed.subscribe(this.state.ticker)
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubscribe}>
+          <input type="text" onChange={this.inputHandleChange}/>
+          <button type="submit">Subscribe</button>
+        </form>
+          <table>
+              <thead>
+                  {/* blah blah table header */}
+                  <tr>
+                      <th>Symbol</th>
+                      <th>Open</th>
+                      <th>Last</th>
+                      <th>Unsubscribe</th>
+                  </tr>
+              </thead>
+                <TickerTable />
+          </table>
+      </div>
+    )
+  }
+}
 
 export default App;
